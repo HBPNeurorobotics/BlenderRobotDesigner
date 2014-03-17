7cd84bd1690a8946ab5acfea62d34c2d24165313
Stefan Ulbrich <_at_kit_edu>
2014-03-17 11:43:25 +0100

    fixed problem in updateKinematics
    Now iterates over the names of children instead of over the children
    themselves

--- mmm.py
+++ mmm.py
@@ -19,6 +19,15 @@ def read(filepath):
     root=doc.getroot()
     tolower(root)
     names=[e.get("name") for e in root.findall(".//jointorder/joint")]
+    missing = []
+    for i in names:
+        if not i in bpy.context.object.data.bones.keys():
+            print("Could not find joint: %s" % i)
+            names.remove(i)
+            missing.append(i)
+
+    print(missing)
+
     timestamps=[float(e.text.strip()) for e in root.findall(".//motionframes/motionframe/timestep")]
     root_positions=[[float(i)*scale_factor for i in e.text.strip().split()] for e in root.findall(".//motionframes/motionframe/rootposition")]
     root_rotations=[[float(i) for i in e.text.strip().split()] for e in root.findall(".//motionframes/motionframe/rootrotation")]
@@ -27,9 +36,17 @@ def read(filepath):
     print(len(root_positions), len(root_rotations))
     bpy.ops.object.mode_set(mode='OBJECT')
 
+    lastFrame = start -8
+
     for [i,[timestamp,root_position,root_rotation,joint_position]] in enumerate(zip(timestamps,root_positions,root_rotations,joint_positions)):
 
+
         bpy.context.scene.frame_current = start + timestamp * fps * 10
+        if bpy.context.scene.frame_current - lastFrame < 4: #or bpy.context.scene.frame_current > 100:
+            print('Skipping')
+            continue
+        lastFrame = bpy.context.scene.frame_current
+
         print(bpy.context.scene.frame_current)
         bpy.context.active_object.location = Vector(root_position)
         bpy.context.active_object.rotation_euler = Euler(root_rotation, "XYZ")
@@ -42,7 +59,10 @@ def read(filepath):
         for [x,value] in enumerate(joint_position):
             if x < len(names):
                 bpy.ops.roboteditor.selectbone(boneName = names[x])
-                bpy.context.active_bone.RobotEditor.theta.value=value/pi*180.0
+                try:
+                    bpy.context.active_bone.RobotEditor.theta.value=value/pi*180.0
+                except KeyError:
+                    print("Error updating %s" %s)
                 print(names[x], value/pi*180, bpy.context.active_bone.RobotEditor.theta.value)
 
         bpy.ops.object.mode_set(mode='POSE')
