2e9694df23440fbfb9716dd7000316466bd4b201
Stefan Ulbrich <_at_kit_edu>
2014-03-19 18:38:52 +0100

    Fixed the collada export again

--- fix.py
+++ fix.py
@@ -12,9 +12,11 @@ def fixCollada(in_filename,out_filename):
             if not element == None: # sometimes, element is None
                 matrix=etree.Element("{http://www.collada.org/2005/11/COLLADASchema}matrix")
                 bone = obj.parent.data.bones[obj.parent_bone]
-                matrix.text = " ".join([str(j) for i in bone.matrix_local.inverted() for j in i])
-                #matrix.text = " ".join([str(j) for i in list(obj.matrix_parent_inverse) for j in i])
+                #matrix.text = " ".join([str(j) for i in bone.matrix_local.inverted() for j in i])
+                matrix.set('sid','Inverted')
+                matrix.text = " ".join([str(j) for i in list(obj.matrix_parent_inverse) for j in i])
                 element.insert(0,matrix)
 
     doc.write(out_filename, encoding="UTF-8")
+    doc.write("test.xml", encoding="UTF-8")
 
