6a89ea3762e8a19f6788e22c87fa7f28232c32e0
Stefan Ulbrich <_at_kit_edu>
2014-03-22 17:52:12 +0100

    Reverted fix.py
    Reason: Initial version was correct (see comments in file).
    Right now, the model has to be in a rest position for export. the
    Transformation of the joint itself is not exported into the scene graph

--- fix.py
+++ fix.py
@@ -8,13 +8,20 @@ def fixCollada(in_filename,out_filename):
     for obj in [i for i in bpy.data.objects if i.type=='MESH']:
          print(obj.name)
          if obj.parent is not None:
-            element = root.find('.//{http://www.collada.org/2005/11/COLLADASchema}node[@name="%s"]'%obj.name)
+            element = root.find('.//{http://www.collada.org/2005/11/COLLADASchema}node[@name="%s"][@type="NODE"]'%obj.name)
             if not element == None: # sometimes, element is None
-                matrix=etree.Element("{http://www.collada.org/2005/11/COLLADASchema}matrix")
+                # Latest discovery: bpy.types.object.matrix_local_inverse() gives only the matrix at the time of parenting!
+                # bpy.types.bone.matrix_local() gives the matrix of the bone at rest position!
+                ns = "{http://www.collada.org/2005/11/COLLADASchema}"
+                matrix=etree.Element(ns+"matrix")
                 bone = obj.parent.data.bones[obj.parent_bone]
-                #matrix.text = " ".join([str(j) for i in bone.matrix_local.inverted() for j in i])
+                if bone.RobotEditor.jointMode == "REVOLUTE":
+                    # TODO: Place the transformation for the joint here!
+                    pass
+
+                matrix.text = " ".join([str(j) for i in bone.matrix_local.inverted() for j in i])
                 matrix.set('sid','Inverted')
-                matrix.text = " ".join([str(j) for i in list(obj.matrix_parent_inverse) for j in i])
+                # matrix.text = " ".join([str(j) for i in list(obj.matrix_local_inverse) for j in i])
                 element.insert(0,matrix)
 
     doc.write(out_filename, encoding="UTF-8")
