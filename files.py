646e932aa6d1295684aa85505f726735dca4c558
Michael Bechtel <ulchl@student.kit.edu>
2014-09-26 11:36:58 +0200

    Experimental fix for physics frames

--- files.py
+++ files.py
@@ -177,7 +177,9 @@ class RobotEditor_exportCollada(bpy.types.Operator):
 
         massFrames = [obj for obj in bpy.data.objects if obj.RobotEditor.tag == 'PHYSICS_FRAME' and not obj.parent_bone is '']
         for frame in massFrames:
-            transform = frame.parent.data.bones[frame.parent_bone].matrix_local.inverted() * frame.matrix_local
+            #transform = frame.parent.data.bones[frame.parent_bone].matrix_local.inverted() * frame.matrix_local
+            poseBone = arm.pose.bones[boneName]
+            transform = poseBone.matrix.inverted()*arm.matrix_world.inverted()*frame.matrix_world
             frameTrafos = []
             frameTrafos.append(tuple(v for v in transform.translation))
             frameRotation = transform.to_euler()
