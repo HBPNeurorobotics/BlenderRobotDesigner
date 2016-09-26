54c0014e0dce6787a28b08ea456919d71d2068d8
gchen2016 <gchen2016@hotmail.com>
2016-09-26 09:14:43 +0200

    Fix pyxb version error & add sdf_model.xsd and sdf_model_dom.py

--- robot_designer_plugin/export/urdf/generic/urdf_tree.py
+++ robot_designer_plugin/export/urdf/generic/urdf_tree.py
@@ -9,7 +9,7 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
+#  modify it under the terms of the GNU General Public Licensexiong
 #  as published by the Free Software Foundation; either version 2
 #  of the License, or (at your option) any later version.
 #
